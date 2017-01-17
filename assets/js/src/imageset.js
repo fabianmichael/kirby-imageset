b3a2c3d5f308cf2dca835eccd25fa77307ba9f25
Fabian Michael <hallo@fabianmichael.de>
2017-01-17 13:24:07 +0100

    Re-organize

--- assets/js/src/imageset.js
+++ assets/js/src/imageset.js
@@ -66,40 +66,12 @@
 
   /* =====  Utilities & Helper Functions  =================================== */
   
-  /* -----  Feature Tests  -------------------------------------------------- */
-
-  var isOperaMini                 = (Object.prototype.toString.call(window.operamini) === "[object OperaMini]");
+  /* -----  Polyfills &  ---------------------------------------------------- */
       
-  var isSafari                    = (ua.indexOf("Safari") !== -1 && ua.indexOf("Chrome") === -1);
-
-  var supportsMutationObserver    = !!window[_MutationObserver];
-
-  var supportsCanvas              = !!window.CanvasRenderingContext2D;
-
-  var supportsPixelatedImages     = ('imageRendering' in docElement.style || 'msInterpolationMode' in docElement.style);
-
   // Shim layer with setTimeout fallback. Look only for unprefixed
   // requestAnimationFrame, because all modern browsern already removed the
   // prefix.
   var rAF = window.requestAnimationFrame || function(fn) { setTimeout(fn, 1000/60); };
-
-  // var supportsObjectFit,
-  //     supportsObjectPosition;
-
-  // (function() {
-  //   var test = function(props, style) {
-  //     for(var i = 0, l = props.length; i < l; i++) {
-  //       var prop = props[i];
-  //       if(prop in style) return true;
-  //     }
-  //     return false;
-  //   };
-
-  //   var style = window.getComputedStyle(document.createElement('img'), null);
-    
-  //   supportsObjectFit      = test(['-o-object-fit', 'object-fit'], style);
-  //   supportsObjectPosition = supportsObjectFit ? test(['-o-object-position', 'object-position'], style) : false; 
-  // })();
 
   /* -----  Utilities  ------------------------------------------------------ */
 
@@ -233,6 +205,8 @@
   // it has not been done by any other script.
   addClass(docElement, __imagesetHasJSClass);
 
+  var isOperaMini = (Object.prototype.toString.call(window.operamini) === "[object OperaMini]");
+
   if(isOperaMini) {
     // Opera Mini has limited DOM Event support and does not
     // work with lazysizes. So we shortcut the loading process
@@ -304,9 +278,15 @@
 
   /* -----  Placeholder Render Functions  ----------------------------------- */
 
-  if(supportsCanvas) {
+  if(!!window.CanvasRenderingContext2D) {
+    // only register placeholder rendering functions, if
+    // canvas is supported by the browser.
 
-  /* ·····  Mosaic  ························································· */
+    /* ···  Mosaic  ························································· */
+
+
+    var isSafari                    = (ua.indexOf("Safari") !== -1 && ua.indexOf("Chrome") === -1);
+    var supportsPixelatedImages     = ('imageRendering' in docElement.style || 'msInterpolationMode' in docElement.style);
 
     if(!supportsPixelatedImages || isSafari) {
 
@@ -346,7 +326,7 @@
       };
     }
 
-  /* ·····  Blurred & LQIP  ················································· */
+    /* ···  Blurred & LQIP  ················································· */
 
     var applyPlaceholderBlur = function(el, radius, mul_sum, shg_sum) {
 
@@ -376,194 +356,195 @@
       };
 
       imageLoaded(source, function() { process(); });
+
     };
 
     placeholderRenderer.blurred = function(el) { applyPlaceholderBlur(el, 15, 512, 17); };
     placeholderRenderer.lqip    = function(el) { applyPlaceholderBlur(el,  7, 512, 15); };
 
-  /* ·····  Triangles  ······················································ */
+    /* ···  Triangles  ······················································ */
 
-  var triangleMosaicFilter = function(canvas, side, alpha) {
-      
-    alpha = !!alpha;
-
-    // Canvas Properties
-    var ctx             = canvas.getContext('2d'),
-        imageData       = ctx.getImageData(0, 0, canvas.width, canvas.height),
-        pixels          = imageData.data,
-        imageDataWidth  = imageData.width,
-        imageDataHeight = imageData.height,
-        xMax            = imageDataWidth  - 1,
-        yMax            = imageDataHeight - 1;
+    var triangleMosaicFilter = function(canvas, side, alpha) {
         
-    // Triangle Properties
-    var height         = Math.round(side * (Math.sqrt(3)/2)), // Triangle height.
-        halfHeight     = height / 2,
-        halfSide       = side   / 2;   
+      alpha = !!alpha;
 
-    //Update canvas if needed (HiDPI/Retina screens)
-    var pixelRatio     = fixCanvasResolution(canvas, ctx);
-    
-    // Utility functions
-    var drawTriangle  = function(x, y, stroke, directionRight) {
-      directionRight = directionRight || false;
-      var xTip  = directionRight ? height : 0,
-          xBase = directionRight ? 0 : height;
-      ctx.translate(x, y);
-      ctx.beginPath();
-      ctx.moveTo(xBase, 0);
-      ctx.lineTo(xTip, halfSide);
-      ctx.lineTo(xBase, side);
-      ctx.fill();
-      ctx.closePath();
-      ctx.translate(-x, -y);
-    };
+      // Canvas Properties
+      var ctx             = canvas.getContext('2d'),
+          imageData       = ctx.getImageData(0, 0, canvas.width, canvas.height),
+          pixels          = imageData.data,
+          imageDataWidth  = imageData.width,
+          imageDataHeight = imageData.height,
+          xMax            = imageDataWidth  - 1,
+          yMax            = imageDataHeight - 1;
+          
+      // Triangle Properties
+      var height         = Math.round(side * (Math.sqrt(3)/2)), // Triangle height.
+          halfHeight     = height / 2,
+          halfSide       = side   / 2;   
 
-    // Utility functions
-    var getColor = function(x, y) {
-      var colorOffset = y * imageDataWidth * 4 + x * 4;
-      return pixels.slice(colorOffset, colorOffset + 4);
-    };
+      //Update canvas if needed (HiDPI/Retina screens)
+      var pixelRatio     = fixCanvasResolution(canvas, ctx);
+      
+      // Utility functions
+      var drawTriangle  = function(x, y, stroke, directionRight) {
+        directionRight = directionRight || false;
+        var xTip  = directionRight ? height : 0,
+            xBase = directionRight ? 0 : height;
+        ctx.translate(x, y);
+        ctx.beginPath();
+        ctx.moveTo(xBase, 0);
+        ctx.lineTo(xTip, halfSide);
+        ctx.lineTo(xBase, side);
+        ctx.fill();
+        ctx.closePath();
+        ctx.translate(-x, -y);
+      };
 
-    var getAlpha = function(x, y) { return pixels[y * imageDataWidth * 4 + x * 4 + 3]; };
+      // Utility functions
+      var getColor = function(x, y) {
+        var colorOffset = y * imageDataWidth * 4 + x * 4;
+        return pixels.slice(colorOffset, colorOffset + 4);
+      };
 
-    var getAverageAlphaFromPoints = function(points) {
-      var alpha = 0, i = 0, len = points.length;
-      for(; i < len; i++) {
-        alpha += pixels[points[i][1] * imageDataWidth * 4 + points[i][0] * 4 + 3];
+      var getAlpha = function(x, y) { return pixels[y * imageDataWidth * 4 + x * 4 + 3]; };
+
+      var getAverageAlphaFromPoints = function(points) {
+        var alpha = 0, i = 0, len = points.length;
+        for(; i < len; i++) {
+          alpha += pixels[points[i][1] * imageDataWidth * 4 + points[i][0] * 4 + 3];
+        }
+        return alpha / len;
+      };
+
+      var rgb  = function(color) { return "rgb(" + color.slice(0, 3).join(",") + ")"; };
+      var rgba = function(color) { color[3] /= 255; return "rgba(" + color.join(",") + ")"; };
+      
+      var sanitizeX = function(x) { return Math.max(0, Math.min(Math.round(x), xMax)); };
+      var sanitizeY = function(y) { return Math.max(0, Math.min(Math.round(y), yMax)); };
+
+      var stepX, xSteps = Math.ceil(imageDataWidth  / height) + 1, // make sure, that canvas is
+          stepY, ySteps = Math.ceil(imageDataHeight / side)   + 1, // completely filled.
+          posX, posY, sanitizedPosX, sanitizedPosY,
+          rectColor,
+          rectColorPosY,
+          trianglePosY,
+          triangleBaseX,
+          triangleTipX,
+          triangleColor,
+          triangleColorPosY,
+          triangleColorPosX,
+          sanitizedTriangleCenterX,
+          sanitizedTriangleCenterY,
+          trianglePointsRight,
+          points,
+          averageAlpha,
+          i;
+
+
+      if(alpha) {
+        // Generate Alpha Mask
+        for(stepY = 0; stepY < ySteps; stepY++) {
+          posY               = stepY * side;
+          rectColorPosY      = sanitizeY(posY + halfSide);
+          trianglePosY       = posY - halfSide;
+          triangleColorPosY  = sanitizeY(posY);   
+          
+          for(stepX = 0; stepX < xSteps; stepX++) {
+            posX = stepX * height;
+            trianglePointsRight = stepX % 2 !== 0;
+            sanitizedPosX       = sanitizeX(posX);
+            sanitizedPosY       = sanitizeY(posY);
+            
+            // Get average alpha for rect and draw it
+            triangleTipX             = sanitizeX(trianglePointsRight ? posX + height - 1 : posX);
+            triangleBaseX            = sanitizeX(trianglePointsRight ? posX : posX + height - 1);
+            sanitizedTriangleCenterX = sanitizeX(posX + halfHeight);
+            sanitizedTriangleCenterY = sanitizeY(posY + halfSide);
+
+            // For calculating alpha transparency, we’re using
+            // the average color of the area covered by
+            // triangles and rects. Although it’s slower than
+            // picking the color value of a single pixel,
+            // results are way better.
+            points = [
+              [ triangleBaseX            , sanitizedPosY              ],
+              [ triangleTipX             , sanitizedTriangleCenterY   ],
+              [ triangleBaseX            , sanitizeY(posY + side - 1) ],
+              [ sanitizedTriangleCenterX , sanitizedTriangleCenterY   ],
+              [ sanitizedTriangleCenterX , sanitizedTriangleCenterY   ],
+            ];
+
+            averageAlpha  = Math.round(getAverageAlphaFromPoints(points));
+            ctx.fillStyle = rgba([averageAlpha, 0, 0, 255]);
+            ctx.fillRect(posX, posY, height, side);
+
+            // Get average alpha for triangle and draw it
+            points = [
+              [ triangleBaseX            , sanitizeY(posY - halfSide)     ],
+              [ triangleTipX             , sanitizedPosY                  ],
+              [ triangleBaseX            , sanitizeY(posY + halfSide - 1) ],
+              [ sanitizedTriangleCenterX , sanitizedPosY                  ],
+              [ sanitizedTriangleCenterX , sanitizedPosY                  ],
+            ];
+
+            averageAlpha  = Math.round(getAverageAlphaFromPoints(points));
+            ctx.fillStyle = rgba([averageAlpha, 0, 0, 255]);
+            drawTriangle(posX, trianglePosY, false, trianglePointsRight);
+          }
+        }
+
+        // Move red channel to alpha channel
+        var alphaImageData  = ctx.getImageData(0, 0, canvas.width, canvas.height),
+            alphaData       = alphaImageData.data,
+            alphaDataLength = alphaData.length;
+
+        for(i = 0; i < alphaDataLength; i += 4) {
+          alphaData[i + 3] = alphaData[i];
+        }
+
+        ctx.putImageData(alphaImageData, 0, 0);
+
+        // Following instructions will use alpha mask
+        ctx.globalCompositeOperation = "source-atop";
       }
-      return alpha / len;
-    };
 
-    var rgb  = function(color) { return "rgb(" + color.slice(0, 3).join(",") + ")"; };
-    var rgba = function(color) { color[3] /= 255; return "rgba(" + color.join(",") + ")"; };
-    
-    var sanitizeX = function(x) { return Math.max(0, Math.min(Math.round(x), xMax)); };
-    var sanitizeY = function(y) { return Math.max(0, Math.min(Math.round(y), yMax)); };
-
-    var stepX, xSteps = Math.ceil(imageDataWidth  / height) + 1, // make sure, that canvas is
-        stepY, ySteps = Math.ceil(imageDataHeight / side)   + 1, // completely filled.
-        posX, posY, sanitizedPosX, sanitizedPosY,
-        rectColor,
-        rectColorPosY,
-        trianglePosY,
-        triangleBaseX,
-        triangleTipX,
-        triangleColor,
-        triangleColorPosY,
-        triangleColorPosX,
-        sanitizedTriangleCenterX,
-        sanitizedTriangleCenterY,
-        trianglePointsRight,
-        points,
-        averageAlpha,
-        i;
-
-
-    if(alpha) {
-      // Generate Alpha Mask
+      // Draw the final triangle mosaic
       for(stepY = 0; stepY < ySteps; stepY++) {
         posY               = stepY * side;
         rectColorPosY      = sanitizeY(posY + halfSide);
         trianglePosY       = posY - halfSide;
         triangleColorPosY  = sanitizeY(posY);   
-        
         for(stepX = 0; stepX < xSteps; stepX++) {
-          posX = stepX * height;
-          trianglePointsRight = stepX % 2 !== 0;
-          sanitizedPosX       = sanitizeX(posX);
-          sanitizedPosY       = sanitizeY(posY);
-          
-          // Get average alpha for rect and draw it
-          triangleTipX             = sanitizeX(trianglePointsRight ? posX + height - 1 : posX);
-          triangleBaseX            = sanitizeX(trianglePointsRight ? posX : posX + height - 1);
-          sanitizedTriangleCenterX = sanitizeX(posX + halfHeight);
-          sanitizedTriangleCenterY = sanitizeY(posY + halfSide);
+          // It’s faster and produces better looking results,
+          // i.e. eliminates artifacts at the edges of triangles
+          // when drawing a rect first and then draw a
+          // triangle that if shifted upwards by half of it’s
+          // height.
+          posX                = stepX * height;
+          triangleColorPosX   = sanitizeX(posX + halfHeight);
+          trianglePointsRight = (stepX % 2 !== 0);
 
-          // For calculating alpha transparency, we’re using
-          // the average color of the area covered by
-          // triangles and rects. Although it’s slower than
-          // picking the color value of a single pixel,
-          // results are way better.
-          points = [
-            [ triangleBaseX            , sanitizedPosY              ],
-            [ triangleTipX             , sanitizedTriangleCenterY   ],
-            [ triangleBaseX            , sanitizeY(posY + side - 1) ],
-            [ sanitizedTriangleCenterX , sanitizedTriangleCenterY   ],
-            [ sanitizedTriangleCenterX , sanitizedTriangleCenterY   ],
-          ];
-
-          averageAlpha  = Math.round(getAverageAlphaFromPoints(points));
-          ctx.fillStyle = rgba([averageAlpha, 0, 0, 255]);
+          // For the final layer, only one color is picked
+          // for the rect and the triangle. This is way faster
+          // than the method used to calculate the alpha mask,
+          // but results are sufficient for a decent quality
+          // of the result.
+          ctx.fillStyle = rgb(getColor(triangleColorPosX, rectColorPosY));
           ctx.fillRect(posX, posY, height, side);
-
-          // Get average alpha for triangle and draw it
-          points = [
-            [ triangleBaseX            , sanitizeY(posY - halfSide)     ],
-            [ triangleTipX             , sanitizedPosY                  ],
-            [ triangleBaseX            , sanitizeY(posY + halfSide - 1) ],
-            [ sanitizedTriangleCenterX , sanitizedPosY                  ],
-            [ sanitizedTriangleCenterX , sanitizedPosY                  ],
-          ];
-
-          averageAlpha  = Math.round(getAverageAlphaFromPoints(points));
-          ctx.fillStyle = rgba([averageAlpha, 0, 0, 255]);
+          
+          ctx.fillStyle = rgb(getColor(triangleColorPosX, triangleColorPosY));
           drawTriangle(posX, trianglePosY, false, trianglePointsRight);
         }
       }
 
-      // Move red channel to alpha channel
-      var alphaImageData  = ctx.getImageData(0, 0, canvas.width, canvas.height),
-          alphaData       = alphaImageData.data,
-          alphaDataLength = alphaData.length;
-
-      for(i = 0; i < alphaDataLength; i += 4) {
-        alphaData[i + 3] = alphaData[i];
+      if(alpha) {
+        // Reset composite operation, in case that other
+        // scripts want to manipulate the canvas further.
+        ctx.globalCompositeOperation = "source-over";
       }
+    };
 
-      ctx.putImageData(alphaImageData, 0, 0);
-
-      // Following instructions will use alpha mask
-      ctx.globalCompositeOperation = "source-atop";
-    }
-
-    // Draw the final triangle mosaic
-    for(stepY = 0; stepY < ySteps; stepY++) {
-      posY               = stepY * side;
-      rectColorPosY      = sanitizeY(posY + halfSide);
-      trianglePosY       = posY - halfSide;
-      triangleColorPosY  = sanitizeY(posY);   
-      for(stepX = 0; stepX < xSteps; stepX++) {
-        // It’s faster and produces better looking results,
-        // i.e. eliminates artifacts at the edges of triangles
-        // when drawing a rect first and then draw a
-        // triangle that if shifted upwards by half of it’s
-        // height.
-        posX                = stepX * height;
-        triangleColorPosX   = sanitizeX(posX + halfHeight);
-        trianglePointsRight = (stepX % 2 !== 0);
-
-        // For the final layer, only one color is picked
-        // for the rect and the triangle. This is way faster
-        // than the method used to calculate the alpha mask,
-        // but results are sufficient for a decent quality
-        // of the result.
-        ctx.fillStyle = rgb(getColor(triangleColorPosX, rectColorPosY));
-        ctx.fillRect(posX, posY, height, side);
-        
-        ctx.fillStyle = rgb(getColor(triangleColorPosX, triangleColorPosY));
-        drawTriangle(posX, trianglePosY, false, trianglePointsRight);
-      }
-    }
-
-    if(alpha) {
-      // Reset composite operation, in case that other
-      // scripts want to manipulate the canvas further.
-      ctx.globalCompositeOperation = "source-over";
-    }
-  };
-
-  placeholderRenderer.triangles = function(el) {
+    placeholderRenderer.triangles = function(el) {
 
       var source   = el[_getElementsByClassName](__placeholderElementClass)[0],
           hasAlpha = hasClass(el, __wrapperAlphaClass);
@@ -654,9 +635,13 @@
 
       imagesetElements = document[_getElementsByClassName](__wrapperPlaceholderClass);
 
-      if(supportsMutationObserver){
+      if(!!window[_MutationObserver]) {
+        // Use MutationObserver to check for new elements,
+        // if supported.
         new window[_MutationObserver]( debouncedCheckImagesets ).observe( docElement, {childList: true, subtree: true, attributes: false, characterData: false } );
       } else {
+        // Otherwise, fallback to Mutation Events and add
+        // a setInterval for as a safety fallback.
         docElement[_addEventListener]('DOMNodeInserted', debouncedCheckImagesets, true);
         docElement[_addEventListener]('DOMAttrModified', debouncedCheckImagesets, true);
         setInterval(debouncedCheckImagesets, 999);
